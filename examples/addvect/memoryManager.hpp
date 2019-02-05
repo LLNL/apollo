a3fae5875d4f8254c5830a785450533a82a51615
Chad Wood <wood67@llnl.gov>
2019-02-05 13:04:38 -0800

    Ported to LLNL's Quartz machine.

--- examples/addvect/memoryManager.hpp
+++ examples/addvect/memoryManager.hpp
@@ -17,7 +17,7 @@
 #define EXAMPLES_MEMORYMANAGER_HPP
 
 #include "RAJA/RAJA.hpp"
-#include "RAJA/util/defines.hpp"
+//#include "RAJA/util/defines.hpp"
 
 /*
   As RAJA does not manage memory we include a general purpose memory
