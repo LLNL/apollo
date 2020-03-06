1b7deb6598eafcee4f9ecf3a7b9bbf7d297f4c11
Chad Wood <wood67@llnl.gov>
2020-03-06 15:47:21 -0800

    adding scripts to do exhaustive sweeps w/new openmp_trace raja policy

--- jobs/job.singlenode.blank.sh
+++ jobs/job.singlenode.blank.sh
@@ -1,28 +1,20 @@
+
+
+# NOTE: This is not done yet...
+
+
 #!/bin/bash
-#SBATCH -p pbatch
-#SBATCH -A asccasc
-#SBATCH --mail-user=wood67@llnl.gov
-#SBATCH --mail-type=ALL
-#SBATCH --requeue
-#SBATCH --exclusive
 #
-#  The following items will need updating at different scales:
-#
-#SBATCH --job-name="APOLLO:COMPARE.1.cleverleaf.test"
-#SBATCH -N 2
-#SBATCH -t 300
-#
-export EXPERIMENT_JOB_TITLE="COMPARE.0001.cleverleaf"  # <-- creates output path!
+export EXPERIMENT_JOB_TITLE="BLANK.1node.cleverleaf"  # <-- creates output path!
 #
 export APPLICATION_RANKS="1"         # ^__ make sure to change SBATCH node counts!
 export SOS_AGGREGATOR_COUNT="1"      # <-- actual aggregator count
-export EXPERIMENT_NODE_COUNT="2"     # <-- is SBATCH -N count, incl/extra agg. node
+export EXPERIMENT_NODE_COUNT="1"     # <-- is SBATCH -N count, incl/extra agg. node
 #
 ###################################################################################
 #
 #  NOTE: Everything below here will get automatically calculated if the above
 #        variables are set correctly.
-#
 #
 #
 export  EXPERIMENT_BASE="/p/lustre2/${USER}/experiments/apollo"
@@ -50,6 +42,7 @@ source ${RETURN_PATH}/common_srun_cmds.sh
 #
 ####
 
+#
 #  Bring over the input deck[s]:
 cp ${HOME}/src/apollo/jobs/cleaf*.in   ${SOS_WORK}
 #
@@ -67,21 +60,20 @@ cd ${SOS_WORK}
 #echo ""
 #echo ">>>> Creating Apollo VIEW and INDEX in the SOS databases..."
 #echo ""
-#
+
 #SOS_SQL=${SQL_APOLLO_VIEW} srun ${SRUN_SQL_EXEC}
 #SOS_SQL=${SQL_APOLLO_INDEX} srun ${SRUN_SQL_EXEC}
+#
 #
 echo ""
 echo ">>>> Launching experiment codes..."
 echo ""
 #
-export CLEVERLEAF_APOLLO_BINARY=" ${SOS_WORK}/bin/cleverleaf-apollo-release "
-export CLEVERLEAF_NORMAL_BINARY=" ${SOS_WORK}/bin/cleverleaf-normal-release "
-export CLEVERLEAF_TRACED_BINARY=" ${SOS_WORK}/bin/cleverleaf-traced-release "
 
+export CLEVERLEAF_APOLLO_BINARY=" ${SOS_WORK}/bin/cleverleaf-apollo-relwithdebinfo "
+export CLEVERLEAF_NORMAL_BINARY=" ${SOS_WORK}/bin/cleverleaf-normal-relwithdebinfo "
+export CLEVERLEAF_TRACED_BINARY=" ${SOS_WORK}/bin/cleverleaf-normal-relwithdebinfo "
 
-
-#export CLEVERLEAF_INPUT="${SOS_WORK}/cleaf_triple_pt_12.in"
 export CLEVERLEAF_INPUT="${SOS_WORK}/cleaf_triple_pt_25.in"
 #export CLEVERLEAF_INPUT="${SOS_WORK}/cleaf_triple_pt_50.in"
 #export CLEVERLEAF_INPUT="${SOS_WORK}/cleaf_triple_pt_100.in"
@@ -90,17 +82,22 @@ export CLEVERLEAF_INPUT="${SOS_WORK}/cleaf_triple_pt_25.in"
 
 export SRUN_CLEVERLEAF=" "
 export SRUN_CLEVERLEAF+=" --cpu-bind=none "
-export SRUN_CLEVERLEAF+=" -c 36 "
+export SRUN_CLEVERLEAF+=" -c 32 "
 export SRUN_CLEVERLEAF+=" -o ${SOS_WORK}/output/cleverleaf.%4t.stdout "
 export SRUN_CLEVERLEAF+=" -N ${WORK_NODE_COUNT} "
 export SRUN_CLEVERLEAF+=" -n ${APPLICATION_RANKS} "
 export SRUN_CLEVERLEAF+=" -r 1 "
 
-echo ">>>> Comparing cleverleaf-normal and cleverleaf-apollo..."
-
+##### --- OpenMP Settings ---
+export KMP_WARNINGS="0"
+export KMP_AFFINITY="noverbose,nowarnings,norespect,granularity=fine,explicit"
+export KMP_AFFINITY="${KMP_AFFINITY},proclist=[0,1,2,3,4,5,6,7,8,9,10,11"
+export KMP_AFFINITY="${KMP_AFFINITY},12,13,14,15,16,17,18,19,20,21,22,23"
+export KMP_AFFINITY="${KMP_AFFINITY},24,25,26,27,28,29,30,31,32,33,34,35]"
 echo ""
-echo "========== EXPERIMENTS STARTING =========="
+echo "KMP_AFFINITY=${KMP_AFFINITY}"
 echo ""
+##### --- OpenMP Settings ---
 
 function wipe_all_sos_data_from_database() {
     SOS_SQL=${SQL_DELETE_VALS} srun ${SRUN_SQL_EXEC}
@@ -119,69 +116,18 @@ function run_cleverleaf_with_model() {
     cd ${SOS_WORK}
 }
 
-##### --- OpenMP Settings ---
-# General:
-export KMP_WARNINGS="0"
-export KMP_AFFINITY="noverbose,nowarnings,norespect,granularity=fine,explicit"
-export KMP_AFFINITY="${KMP_AFFINITY},proclist=[0,1,2,3,4,5,6,7,8,9,10,11"
-export KMP_AFFINITY="${KMP_AFFINITY},12,13,14,15,16,17,18,19,20,21,22,23"
-export KMP_AFFINITY="${KMP_AFFINITY},24,25,26,27,28,29,30,31,32,33,34,35]"
-##### --- OpenMP Settings ---
 
-set +m
-
-#srun ${SRUN_CONTROLLER_START} &
-#sleep 4
-
-
-
-export OPENMP_TRACE_OUTPUT_FILE="${SOS_WORK}"
-
-#
-run_cleverleaf_with_model ${CLEVERLEAF_NORMAL_BINARY} ${CLEVERLEAF_INPUT} "normal---------default"
-run_cleverleaf_with_model ${CLEVERLEAF_TRACED_BINARY} ${CLEVERLEAF_INPUT} "traced---------default"
-run_cleverleaf_with_model ${CLEVERLEAF_APOLLO_BINARY} ${CLEVERLEAF_INPUT} "model.static.0.default"
-#
-
-export OMP_NUM_THREADS=32
-export OMP_SCHEDULE="static"
-export OPENMP_TRACE_OUTPUT_FILE="${SOS_WORK}/output/traced.32.static.csv"
-export OPENMP_TRACE_AS_POLICY=6
-# NOTE: Soon to be "OPENMP_TRACE_AS_POLICY
-run_cleverleaf_with_model ${CLEVERLEAF_NORMAL_BINARY} ${CLEVERLEAF_INPUT} "normal.${OMP_NUM_THREADS}.${OMP_SCHEDULE}"
-run_cleverleaf_with_model ${CLEVERLEAF_TRACED_BINARY} ${CLEVERLEAF_INPUT} "traced.${OMP_NUM_THREADS}.${OMP_SCHEDULE}"
-#
+export APOLLO_INIT_MODEL=${SOS_WORK}/model.static.0.default
 cd ${SOS_WORK}
-
-echo ""
-echo "========== EXPERIMENTS COMPLETE =========="
-echo ""
-
-#echo ""
-#echo ">>>> Bringing down the controller and waiting for 5 seconds (you may see 'kill' output)..."
-#echo ""
-#printf "== CONTROLLER: STOP\n" >> ./output/controller.out
-#srun ${SRUN_CONTROLLER_STOP}
-#echo ""
-#sleep 5
 
 #####
 #
 source ${RETURN_PATH}/common_parting.sh
 #
-set -m
-echo " >>>>"
-echo " >>>>"
-echo " >>>> Press ENTER or wait 120 seconds to shut down SOS.   (C-c to stay interactive)"
-echo " >>>>"
-read -t 120 -p " >>>> "
 echo ""
-echo " *OK* Shutting down interactive experiment environment..."
+echo "OK!  You are now ready to run Apollo experiments."
 echo ""
-${SOS_WORK}/sosd_stop.sh
-sleep 8
-echo ""
-echo ""
-echo "--- Done! End of job script. ---"
 #
+#####
+
 # EOF
