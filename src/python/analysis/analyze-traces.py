d6cf973cf3c22f8365ab76d6e357cca3e047b717
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2023-04-24 13:29:29 -0700

    Fix bug in analysis scripts
    - Compute correctly speedup per rank when comparing static with
      theoretical optimal policies

--- src/python/analysis/analyze-traces.py
+++ src/python/analysis/analyze-traces.py
@@ -59,14 +59,14 @@ def compute_execution_time(rank, data, xtime_per_pol, init_model, trained_model=
 
 
 def compute_speedup(xtime_per_pol, model, min_xtime_static_pol):
-    print('%s speedup vs defaults (Static,0 / %s): %.2f' %
+    print('%s speedup vs defaults (Static,0 / %s): %.3f' %
           (
               model,
               model,
               sum(xtime_per_pol['Static,0'])/sum(xtime_per_pol[model]),
           )
           )
-    print('%s speedup vs best static (%s / %s): %.2f' %
+    print('%s speedup vs best static (%s / %s): %.3f' %
           (
               model,
               min_xtime_static_pol,
@@ -195,58 +195,57 @@ def main():
             csv_map.values(), ignore_index=True, sort=True)
 
     print()
-    xtime_per_pol = {}
-    for i in range(args.nranks):
-        # Print execution time for Static training.
+    # Compute execution times per rank and per policy.
+    for rank in range(args.nranks):
+        xtime_per_pol = {}
         for j in range(args.nstatic):
             compute_execution_time(
-                i, data_map['Static'], xtime_per_pol, 'Static,%d' % (j))
-        # Print execution time for RoundRobin training plus DecisionTree execution.
+                rank, data_map['Static'], xtime_per_pol, 'Static,%d' % (j))
         if args.rr:
             compute_execution_time(
-                i, data_map['RoundRobin'], xtime_per_pol, 'RoundRobin', 'DecisionTree')
-        # Print execution time for Random training plus DecisionTree execution.
+                rank, data_map['RoundRobin'], xtime_per_pol, 'RoundRobin', 'DecisionTree')
         if args.random:
             compute_execution_time(
-                i, data_map['Random'], xtime_per_pol, 'Random', 'DecisionTree')
+                rank, data_map['Random'], xtime_per_pol, 'Random', 'DecisionTree')
         if args.load:
-            compute_execution_time(i, data_map['Load'], xtime_per_pol, 'Load')
+            compute_execution_time(
+                rank, data_map['Load'], xtime_per_pol, 'Load')
         if args.opt:
             compute_execution_time(
-                i, data_map['Optimal'], xtime_per_pol, 'Optimal')
+                rank, data_map['Optimal'], xtime_per_pol, 'Optimal')
 
-    print()
-    print('=== Computing optimal policy selection...')
-    for i in range(args.nranks):
+        print('=== Computing optimal policy selection...')
         t1 = time.perf_counter()
-        # Find optimal policy selection (fastest way): select by rank equals i and training equals to Static;
+        # Find optimal policy selection (fastest way): select by rank and training equals to Static;
         # sort by xtime;
         # drop_duplicates grouped by region, idx to keep minimum;
         # set index to (region, idx) as the unique identifier
         # sort by index for easy visualization and comparison
 
         # &(data_map['Static']['training'].str.contains('Static'))]\
-        opt = data_map['Static'].loc[(data_map['Static']['rankid'] == i)]\
+        opt = data_map['Static'].loc[(data_map['Static']['rankid'] == rank)]\
             .sort_values(by=['xtime'])\
             .drop_duplicates(['region', 'idx'], keep='first')\
             .set_index(['region', 'idx'])\
             .sort_index()
         t2 = time.perf_counter()
-        # print('=========== OPT =============\n', opt.to_string(), '\n=============================',
-        #        file=open('opt.txt','w'))
-        assert len(opt.index) == len(
-            data_map['Static'].index)/args.nstatic, 'Data error'
+        # with open('opt.txt', 'w') as f:
+        #    print('=========== OPT =============\n', opt.to_string(), '\n=============================',
+        #          file=f)
+        # NOTE: This assertion does not hold if regions do not have the same number of policies.
+        # assert len(opt.index) == len(
+        #    data_map['Static'].index)/args.nstatic, 'Data error'
 
-        print('=== Writing out optimal policy selection...')
         if args.write_opt:
+            print('=== Writing out optimal policy selection...')
             index_set = set(opt.index.get_level_values(0))
             for region in index_set:
                 opt_policies = [str(x) for x in opt.loc[region, 'policy']]
-                with open('opt-%s-rank-%s.txt' % (region, i), 'w') as f:
+                with open('opt-%s-rank-%s.txt' % (region, rank), 'w') as f:
                     f.write(','.join(opt_policies))
 
         #print('opt-rank-%d time %.6f s' % (i, t2-t1))
-        print('opt-rank-%d xtime %.6f' % (i, sum(opt['xtime'])))
+        print('opt-rank-%d xtime %.6f' % (rank, sum(opt['xtime'])))
         min_xtime_static_pol = min(
             {k: v for k, v in xtime_per_pol.items() if 'Static' in k}, key=xtime_per_pol.get)
 
@@ -266,19 +265,19 @@ def main():
         if args.rr:
             accuracy['RoundRobin'] =\
                 compute_accuracy(
-                    i, data_map['RoundRobin'], opt, args.nstatic+1, DIFF_PCT, 'RoundRobin', 'DecisionTree')
+                    rank, data_map['RoundRobin'], opt, args.nstatic+1, DIFF_PCT, 'RoundRobin', 'DecisionTree')
         if args.random:
             accuracy['Random'] =\
                 compute_accuracy(
-                    i, data_map['Random'], opt, args.nstatic+1, DIFF_PCT, 'Random', 'DecisionTree')
+                    rank, data_map['Random'], opt, args.nstatic+1, DIFF_PCT, 'Random', 'DecisionTree')
         if args.load:
             accuracy['Load'] =\
                 compute_accuracy(
-                    i, data_map['Load'], opt, args.nstatic+1, DIFF_PCT, 'Load', 'Load')
+                    rank, data_map['Load'], opt, args.nstatic+1, DIFF_PCT, 'Load', 'Load')
         if args.opt:
             accuracy['Optimal'] =\
                 compute_accuracy(
-                    i, data_map['Optimal'], opt, args.nstatic+1, DIFF_PCT, 'Optimal', 'Optimal')
+                    rank, data_map['Optimal'], opt, args.nstatic+1, DIFF_PCT, 'Optimal', 'Optimal')
 
         if args.plot:
             fig, ax = plt.subplots(figsize=(10, 6))
